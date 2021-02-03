6cc5a470ff3713bd39f2e07084ed6cb2c7048c25
Eric <61775837+eric8979@users.noreply.github.com>
2021-02-03 12:25:42 +0900

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+// actions - (using lodash)
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach(id => dispatch(fetchUser(id)))
+		.value(); //execute
+};
+
+export const fetchPosts = () => async dispatch => {
+	const response = await jsonPlaceholder.get('/posts');
+	dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// USING -.memoize FROM LODASH
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// // _funcName === private function
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+// 	dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
